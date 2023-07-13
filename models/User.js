41269045ae9dcd48b2846c3baeca123549216aa2
BanditHeelerPDX <kirk.alexander.steele@gmail.com>
2023-07-12 23:29:45 -0700

    case issue

--- models/User.js
+++ models/User.js
@@ -1,46 +1,47 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const UserSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true,
-        min: 5,
-        max: 25,
-        unique: true
+const UserSchema = new mongoose.Schema(
+  {
+    userName: {
+      type: String,
+      required: true,
+      min: 5,
+      max: 25,
+      unique: true,
     },
     email: {
-        type: String,
-        required: true,
-        unique: true,
-        max: 75
+      type: String,
+      required: true,
+      unique: true,
+      max: 75,
     },
     password: {
-        type: String,
-        required: true,
-        min: 10
+      type: String,
+      required: true,
+      min: 10,
     },
     profilePicture: {
-        type: String,
-        default: ""
+      type: String,
+      default: "",
     },
     coverPicture: {
-        type: String,
-        default: ""
+      type: String,
+      default: "",
     },
     friends: {
-        type: Array,
-        default: []
+      type: Array,
+      default: [],
     },
     minions: {
-        type: Array,
-        default: []
+      type: Array,
+      default: [],
     },
     isAdmin: {
-        type: Boolean,
-        default: false
+      type: Boolean,
+      default: false,
     },
-},
-{timestamps: true}
+  },
+  { timestamps: true }
 );
 
 module.exports = mongoose.model("User", UserSchema);
